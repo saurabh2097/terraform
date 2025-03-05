3570cda3a6e3940e068abb7d9440bcab9af59582
saurabh2097 <saurabhchimankar97@gmail.com>
2025-03-05 20:25:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+region == "us-east-1"
+}
+
+resource "aws_instance" "one" {
+  ami           = "ami-02a53b0d62d37a757"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "hcp-server"
+  }
+}
