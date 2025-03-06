d5e6e7499fbc6e0e51a56c0fc1522c7c8ac4b644
saurabh2097 <saurabhchimankar97@gmail.com>
2025-03-06 20:48:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-  ami           = "ami-02a53b0d62d37a757"
+  ami      = "ami-02a53b0d62d37a757"
   instance_type = "t2.micro"
   tags = {
-    Name = "hcp-server"
+    Name = "module-server"
   }
 }
